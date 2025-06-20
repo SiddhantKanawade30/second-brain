c99d26cf61a320a6ab0c952d2a9c6109ecb8e90c
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2025-06-20 21:42:49 +0530

    progress v1

---
+++ src/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+import { Schema , model } from "mongoose"
+
+const userSchema = new Schema({
+    email : {type: String, unique: true},
+    password : {type : String , unique : true},
+    fistName : String,
+})
+
+const contentSchema = new Schema({
+    title : {type: String, unique: true} ,
+    link : {type: String, unique: true},
+    tags : [{type : mongoose.Types.ObjectId , ref:"tags"}],
+
+        userId : [{
+            type : mongoose.Types.ObjectId,
+            ref: "User",
+            required : true
+        }]
+})
+
+export const userModel = model("User",userSchema)
+export const contentModel = model("Content",contentSchema)
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express"
+const app = express()
+import jwt from "jsonwebtoken"
+import { Request,Response } from "express"
+
+
+
+app.post("api/v1/signup", async function(req:Request,res:Response){
+    
+})
+
+
+
+
+
+
+
